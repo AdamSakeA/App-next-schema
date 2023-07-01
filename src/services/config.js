e2c10dfe1c7d5f620819097a11a31f6c5889bc97
AdamSakeA <adamsake8@gmail.com>
2023-07-02 00:06:43 +0700

    fixed: next auth

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,6 @@
 const ConfigUrl = {
-  baseURL: "http://localhost:3000/api/",
+  // baseURL: "http://localhost:3000/api/",
+  baseURL: "https://app-next-schema.vercel.app/api/",
 };
 
 export default ConfigUrl;
