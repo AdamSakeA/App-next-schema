6447372884bca6d0517b3f1eb05067cee1956d7c
AdamSakeA <adamsake8@gmail.com>
2023-07-12 01:42:41 +0700

    lala

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 const ConfigUrl = {
-  baseURL: "http://localhost:3000/api/",
-  // baseURL: "https://app-next-schema.vercel.app/api/",
+  // baseURL: "http://localhost:3000/api/",
+  baseURL: "https://app-next-schema.vercel.app/api/",
 };
 
 export default ConfigUrl;
